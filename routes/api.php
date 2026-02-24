d7afad13b99cb46e5f84dfc78b6accb430b5b0ae
Ammad Sarfraz <asyousufzai@hotmail.com>
2026-02-25 01:50:39 +0500

    refactor(api): moved AuthController to Api/V1 namespace for versioned API structure

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\AuthController;
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
+Route::prefix('v1')->group(function () {
+
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
+
 });
